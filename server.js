bb3b7ec42a2c53c78e32770366aa8ed5c1a67e5d
Shivam Singh <shivamss17.98@gmail.com>
2024-12-21 01:58:51 +0000

    done

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors()); //
 app.use(express.json());
 
 app.get("/api/metadata", async (req, res) => {
